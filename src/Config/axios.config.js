2ea736a8c4583a6f58887c5cd0b86abc6367fc57
EsraaSamyZ <142722769+EsraaSamyZ@users.noreply.github.com>
2023-11-05 07:50:54 +0200

    request interceptor added

--- src/Config/axios.config.js
+++ src/Config/axios.config.js
@@ -1,7 +1,24 @@
 import axios from "axios";
-export const server = axios.create({
+
+const server = axios.create({
     baseURL: "http://localhost:3000",
     headers: {
         "Content-Type": "application/json",
     }
 });
+
+server.interceptors.request.use(
+    config => {
+    //   const token = localStorage.getItem('authToken');
+      const token = "TOKEN";
+      if (token) {
+        config.headers['Authorization'] = 'Bearer ' + token;
+      }
+      return config;
+    },
+    error => {
+      Promise.reject(error);
+    }
+  );
+
+  export { server };
